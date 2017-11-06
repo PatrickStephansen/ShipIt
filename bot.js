b666b138946ae12ccf19982cd40315cbd21795ef
Patrick Stephansen <patrick@entelect.co.za>
2017-11-06 23:48:17 +0200

    Get defensive

--- bot.js
+++ bot.js
@@ -22,14 +22,13 @@ function initBot(args) {
   // Read the current state and choose an action
   let stateFile = require(workingDirectory + "/" + stateFileName);
   let phase = stateFile.Phase;
-  boardSize =
-    stateFile.PlayerMap.Cells[stateFile.PlayerMap.Cells.length - 1].X + 1;
+  boardSize = stateFile.PlayerMap.MapWidth;
 
   if (phase == 1) {
     placeShips(workingDirectory, stateFile);
   }
   if (phase == 2) {
-    fireOrDoNothing(workingDirectory, stateFile);
+    commitViolence(workingDirectory, stateFile);
   }
 }
 
@@ -41,9 +40,7 @@ Destroyer ${boardSize - 1} 0 West
 Submarine 1 ${boardSize - 2} South
 Cruiser 3 ${boardSize - 2} South`;
 
-  fs.writeFile(workingDirectory + "/" + placeShipFileName, payload, function(
-    err
-  ) {
+  fs.writeFile(workingDirectory + "/" + placeShipFileName, payload, function(err) {
     if (err) {
       return console.log(err);
     }
@@ -55,25 +52,60 @@ function placeShip(ship, usedSpots) {
   getRandomSpot;
 }
 
-function fireOrDoNothing(workingDirectory, state) {
-  // Random fires
-  let fire = 1;
-  let target = getNextSingleSpot(state);
+function commitViolence(workingDirectory, state) {
+  const shieldPlace = getShieldPlacement(state);
+  let payload;
+  if (shieldPlace.isGoodIdea) {
+    console.log(`shielding at ${shieldPlace}`);
+    payload = `8,${shieldPlace}
+`;
+  } else {
+    let fire = 1;
+    let target = getNextSingleSpot(state);
+    console.log(`firing at ${target}`);
+    payload = `${fire},${target}
+`;
+  }
 
-  let payload = `${fire},${target}\n`;
-
-  fs.writeFile(workingDirectory + "/" + commandFileName, payload, function(
-    err
-  ) {
+  fs.writeFile(workingDirectory + "/" + commandFileName, payload, function(err) {
     if (err) {
       return console.log(err);
     }
-    console.log("A shot was fired");
   });
 }
 
 function getNextSingleSpot(state) {
   return getRandomSpot();
+}
+
+function getShieldPlacement(state) {
+  const myShield = state.PlayerMap.Owner.Shield;
+
+  if (!myShield.Active && myShield.CurrentRadius) {
+    console.log("Shield time");
+    const myShips = state.PlayerMap.Owner.Ships.filter(s => !s.Destroyed);
+    const shipLocations = [].concat(...myShips.map(s => s.Cells.filter(c => !c.Hit)));
+
+    let bestSpot = { x: 5, y: 5, protected: 0 };
+    for (let x = myShield.CurrentRadius + 1; x < boardSize - myShield.CurrentRadius - 1; x++) {
+      for (let y = myShield.CurrentRadius + 1; y < boardSize - myShield.CurrentRadius - 1; y++) {
+        let protected = shipLocations.filter(
+          s => Math.abs(s.X - x) <= myShield.CurrentRadius && Math.abs(s.Y - y) <= myShield.CurrentRadius
+        ).length;
+        if (protected > bestSpot.protected) {
+          bestSpot = { x: x, y: y, protected: protected };
+        }
+      }
+    }
+    if (bestSpot.protected > 1) {
+      bestSpot.isGoodIdea = true;
+      bestSpot.toString = function() {
+        return `${this.x},${this.y}`;
+      };
+      return bestSpot;
+    }
+  }
+  return { isGoodIdea: false };
 }
 
 function getRandomSpot() {
