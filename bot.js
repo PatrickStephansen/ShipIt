2eddfdb7276bb65bc2f3bddec27d72ce19e1472b
Patrick Stephansen <patrick@entelect.co.za>
2017-11-07 20:40:16 +0200

    Diagonals don't count as adjacent to previous hits

--- bot.js
+++ bot.js
@@ -106,7 +106,7 @@ function getNextSingleSpot(state) {
   let likelyHit = state.OpponentMap.Cells.find(
     c =>
       !(c.Damaged || c.Missed) &&
-      state.OpponentMap.Cells.find(n => n.Damaged && Math.abs(n.X - c.X) < 2 && Math.abs(n.Y - c.Y) < 2)
+      state.OpponentMap.Cells.find(n => n.Damaged && Math.abs(n.X - c.X) + Math.abs(n.Y - c.Y) < 2)
   );
   if (likelyHit) {
     return {
